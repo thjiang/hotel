f198f97a4d87033b80b4a9c66307ad1dec6938e8
thjiang <browserify@qq.com>
2018-02-06 22:28:07 +0800

    header

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,29 +1,62 @@
 <style lang="less" scoped="scoped">
-div {
-    color: red;
-}
+.header {
+    height: 500px;
+    width: 100%;
+    text-align: center;
+    // background: linear-gradient(to right,#4ac1ed 0,#6c8ad7 33%,#ffb991 66%,#ffa8b3 100%);
+    background: linear-gradient(to right,#ffb991 0, #ffa8b3 100%);
 
-.content {
-    color: purple;
-    transform: translateY(40px);
+    .content {
+        width: 850px;
+        margin: 0 auto;
+        .title {
+            padding-top: 80px;
+            font-size: 40px;
+            font-weight: lighter;
+            // line-height: 60px;
+            color: #fff;
+            text-align: left;
+        }
+    }
+    .recommend {
+        width: 850px;
+        margin: 0 auto;
+        text-align: left;
+        li {
+            display: inline;
+            margin-right: 25px;
+            font-size: 14px;
+            color: #fff;
+            cursor: pointer;
+        }
+    }
+    
 }
 </style>
 
 <template>
-    <div>
-        这是{{page}}页面
-        <div class="content">
-            这是一段紫色的字
-        </div>
-    </div>
+    <header class="header">
+        <section class="content">
+            <h3 class="title">让有梦想的人走得更远</h3>
+        </section>
+        <search></search>
+        <ul class="recommend">
+            <li>万豪</li><li>喜达屋</li><li>凯悦</li><li>希尔顿</li><li>洲际</li>
+        </ul>
+    </header>
 </template>
 
 <script>
+import search from './search.vue';
+
 export default {
     data: function() {
         return {
             page: "index"
         };
+    },
+    components: {
+        search
     }
 };
 </script>
---
+++ src/components/search.vue
@@ -0,0 +1,26 @@
+<style lang="less" scoped="scoped">
+    .search-wrap{
+        width: 850px;
+        margin: 30px auto 20px;
+        height: 64px;
+        line-height: 64px;
+        background-color: #fff;
+    }
+</style>
+
+<template>
+    <section class="search-wrap">
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'search',
+    data: function() {
+        return {
+            page: "search"
+        };
+    }
+};
+</script>
